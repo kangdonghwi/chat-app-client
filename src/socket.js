da256d98fcebc5bb44e6cff6cea9e75f3b05cbf2
KANGDONGHWI <rkdehdgnl@naver.com>
2021-05-16 00:06:28 +0900

    process.env rm

--- src/socket.js
+++ src/socket.js
@@ -1,2 +1,2 @@
 import openSocket from "socket.io-client";
-export const socket = openSocket(process.env.REACT_APP_SERVER_URL);
+export const socket = openSocket(`https://sixweeks-server.herokuapp.com/`);
